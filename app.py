270d6fb1be3a7cf60e8597043a797d2e82cb19b1
IRFAN ULLAH KHAN <134633289+programmarself@users.noreply.github.com>
2024-08-27 22:26:29 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ splitter_details = {
 
 # Streamlit app
 st.title("RAG Splitter System")
-st.title('Developed By :Irfan Ullah Khan')
+st.subtitle('Developed By :Irfan Ullah Khan')
 
 # User input for data
 user_data = st.text_area("Enter the data you want to split:", "This is a sample text. Enter your data here...")
